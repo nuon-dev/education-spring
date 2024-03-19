42fd0043e88c5ff65e154cc5cbdcda7d740871cc
jhlee3516 <windsor9762@gmail.com>
2024-03-19 22:41:27 +0900

    first

---
+++ src/main/kotlin/org/nuon/JINHOLEEdemo/JinholeeDemoApplication.kt
@@ -0,0 +1,11 @@
+package org.nuon.JINHOLEEdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JinholeeDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<JinholeeDemoApplication>(*args)
+}
---
+++ src/test/kotlin/org/nuon/JINHOLEEdemo/JinholeeDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.nuon.JINHOLEEdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JinholeeDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
