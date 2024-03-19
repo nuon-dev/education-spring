64e5f13f70a8615c73c2a782cfb697617b139c94
yoonjuyoung <zuyoungee@naver.com>
2024-03-19 22:33:52 +0900

    first class

---
+++ src/main/kotlin/com/example/zuyoungeedemo/ZuyoungeeDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.zuyoungeedemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZuyoungeeDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZuyoungeeDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/zuyoungeedemo/ZuyoungeeDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.zuyoungeedemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZuyoungeeDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
