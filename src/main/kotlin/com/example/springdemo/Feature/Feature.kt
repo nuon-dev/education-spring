dc7669c53c77e44377751674246aba8f55a8bc81
Junho Geum <junhogeum@Junhos-MacBook-Air.local>
2024-03-19 22:29:51 +0900

    test commit

---
+++ src/main/kotlin/com/example/springdemo/Feature/Feature.kt
@@ -0,0 +1,36 @@
+package com.example.springdemo.Feature
+
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Feature {
+    @GetMapping("/index")
+    fun index(
+            @RequestParam message:String
+    ):String{
+
+        return "Hello World! message is $message"
+    }
+
+    @PostMapping("/index")
+    fun post():String{
+        return "post"
+    }
+
+    @PutMapping
+    fun put():String{
+        return "put"
+    }
+
+    @DeleteMapping
+    fun delete():String{
+        return "delete"
+    }
+
+
+}
